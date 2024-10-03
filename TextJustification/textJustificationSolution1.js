5983556f36ea6f143d5f34f8cc8a1a1b8fffdf62
dvirak <dvirak43@vt.edu>
2024-10-03 14:09:32 -0400

    Almost there with Text Justification!

--- TextJustification/textJustificationSolution1.js
+++ TextJustification/textJustificationSolution1.js
@@ -16,64 +16,103 @@ var fullJustify = function (words, maxWidth) {
   let justArray = [];
   let currentArray = [];
   let currentWidth = maxWidth;
-  console.log(words.length);
-  console.log(words[words.length - 1]);
 
   for (const word of words) {
-    console.log(word);
+    console.log("word: " + word);
+    console.log(currentArray);
+    console.log(justArray);
+    console.log("currentWidth: " + currentWidth);
+    console.log("Word Length: " + (word.length + 1));
+
     if (currentWidth - word.length - 1 >= 0) {
+      console.log("WE HEREE 1st IF");
       currentArray.push(word);
       currentWidth -= word.length + 1;
-      console.log("currentArray");
-      console.log(currentArray);
-      console.log("currentWidth = " + currentWidth);
-    } else if (currentWidth - word.length < 0) {
+      // fix this if so that its equal to that ACTUAL LAST INDEX
+      if (word === words[words.length - 1]) {
+        console.log("WE HEREE 2nd IF");
+        justArray.push(currentArray);
+        currentArray = [];
+        currentWidth = maxWidth;
+      }
+    } else if (currentWidth - word.length - 1 < 0) {
+      if (currentWidth - word.length === 0) {
+        console.log("WE HEREE 3rd IF");
+        currentArray.push(word);
+        justArray.push(currentArray);
+        currentArray = [];
+        currentWidth = maxWidth;
+        continue;
+      }
+      console.log("WE HEREE 4th IF");
+
       currentWidth = maxWidth;
       justArray.push(currentArray);
-      console.log("justArray: ");
-      console.log(justArray);
       currentArray = [];
       currentArray.push(word);
-      currentWidth -= word.length;
-      console.log("currentArray");
-      console.log(currentArray);
+      currentWidth -= word.length + 1;
       if (word === words[words.length - 1]) {
+        console.log("WE HEREE 5th IF");
+
         justArray.push(currentArray);
       }
     }
   }
 
+  console.log(justArray);
   // ok so we got the justArray we need, now we need to add spaces
   // so we need to go row by row
   // then in that row, we need to start adding spaces from left to right until we reach our max width
   // when we get to the last word in that row, we need to return to the first word
   // when we reach the last row of justArray, we can just return justArray
   // also if only one word in the line then we can move on to the next row
+  // could i take a row, join it
+  // then subtract length from maxWidth
+  // that difference is the total number of spaces we need
+  // then while difference > 0
+  // for i-row.length
+  // if i === row.length - 1
+  // i = 0
+  // row[0] += " "
 
-  for (const row of justArray) {
+  let retArray = [];
+
+  for (let row of justArray) {
+    console.log(row);
+    console.log(row.join(""));
+
     if (row === justArray[justArray.length - 1]) {
       console.log("WE DID IT");
-      console.log(justArray);
-    }
-    console.log(row);
-    console.log(row.length);
-    let joinedRow = row.join(" ");
-    console.log(joinedRow);
-    console.log(joinedRow.length);
-    // while (joinedRow.length < maxWidth) {
-    for (let i = 0; i < row.length; i++) {
-      console.log("i: " + i);
-      console.log("row[i]: " + row[i]);
-      if (i === row.legnth - 1) {
-        i = 0;
-        row[i] = row[i] += " ";
-        joinedRow = row.join(" ");
-      } else {
-        row[i] = row[i] += " ";
-        joinedRow = row.join(" ");
+      let joinedRow = row.join(" ");
+      console.log("joinedRow: ");
+      console.log(joinedRow);
+      let difference = maxWidth - joinedRow.length;
+      while (difference > 0) {
+        row[row.length - 1] += " ";
+        difference--;
       }
+      retArray.push(row.join(" ").toString());
+      console.log(retArray);
+      return retArray;
     }
-    // }
+    let joinedRow = row.join("");
+    console.log("joinedRow: ");
+    console.log(joinedRow);
+    let difference = maxWidth - joinedRow.length;
+    let index = 0;
+
+    while (difference > 0) {
+      console.log("in while loop");
+      if (index >= row.length - 1) {
+        index = 0;
+      }
+      row[index] = row[index] += " ";
+      index++;
+      difference--;
+    }
+
+    console.log(row.join("").toString());
+    retArray.push(row.join("").toString());
   }
 
   console.log(justArray);
