0d1d868a8a3f72ee8fdbbf7cb84c940ae62dfd3f
dvirak <dvirak43@vt.edu>
2024-10-20 12:23:54 -0400

    threeSumSolution1 CORRECTgit add .!

--- 3Sum/threeSumSolution1.js
+++ 3Sum/threeSumSolution1.js
@@ -1,49 +1,69 @@
-// check for all where i=0, j =1 k = 2-nums.length
-// increase j by 1
+// First sort the array so we know which directions we need to be moving
+// we start with i at 0, j at i + 1, k at last index
+// we start a while loop (j < k)
+// if nums[i] + nums[j] + nums[k] > 0, we move k left
+// if nums[i] + nums[j] + nums[k] < 0, we move j right
+// if nums[i] + nums[j] + nums[k] = 0, we add those numbers to return array
+// then we repeat loop and increase i
+// but we need to check if new i === old i so we dont repeat numbers
+// but wait! we need to check other values at current i before we move on
+// so lets increase j until we have a new j, then go back into while loop
 
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  let i = 0;
-  let j = 1;
-  let k = 2;
   let retArray = [];
 
-  while (i < nums.length - 2) {
-    while (j < nums.length - 1) {
-      if (nums[i] + nums[j] + nums[k] === 0) {
-        retArray.push([nums[i], nums[j], nums[k]]);
-        console.log("retArray updated!");
-        console.log(retArray);
+  nums.sort((a, b) => a - b);
+
+  console.log(nums);
+
+  for (let i = 0; i < nums.length; i++) {
+    // console.log("New Loop")
+    // console.log("i = " + i)
+    // console.log(retArray)
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    let j = i + 1;
+    // console.log("j = " + j)
+    let k = nums.length - 1;
+    // console.log("k = " + k)
+
+    while (j < k) {
+      let total = nums[i] + nums[j] + nums[k];
+      //   console.log("total = " + total)
+      if (total > 0) {
+        k--;
+        // console.log("decrease k")
+        // console.log("K = " + k)
       }
-      j++;
+
+      if (total < 0) {
+        j++;
+        // console.log("increase j")
+        // console.log("j = " + j)
+      }
+
+      if (total === 0) {
+        // console.log("We have a winner!")
+        retArray.push([nums[i], nums[j], nums[k]]);
+        // console.log(retArray)
+
+        j++;
+        // console.log("increase j once")
+        // console.log("j = " + j)
+
+        while (nums[j] === nums[j - 1] && j < k) {
+          // console.log("j loop increase")
+          j++;
+          //   console.log("j = " + j)
+        }
+      }
     }
-  }
-  while (k < nums.length) {
-    if (nums[i] + nums[j] + nums[k] === 0) {
-      retArray.push([nums[i], nums[j], nums[k]]);
-      console.log("retArray updated!");
-      console.log(retArray);
-    }
-    k++;
-  }
-  console.log("k = " + k);
-
-  j += 1;
-
-  console.log("j = " + j);
-
-  i += 1;
-
-  while (i < nums.length - 2) {
-    if (nums[i] + nums[j] + nums[k] === 0) {
-      retArray.push([nums[i], nums[j], nums[k]]);
-      console.log("retArray updated!");
-      console.log(retArray);
-    }
-    i++;
   }
 
   return retArray;
