45380112a0a0a6483c8bfbad26f4d0d5ff39a7aa
dvirak <dvirak43@vt.edu>
2024-11-20 17:33:50 -0500

    MinimumSizeSubarraySolution3

---
+++ MinimumSizeSubarray/MinimumSizeSubarraySolution3.js
@@ -0,0 +1,26 @@
+// Same as previous but a for loop instead of a while loop
+
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+  let minLen = Infinity;
+  let left = 0;
+  let curSum = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+    curSum += nums[right];
+
+    while (curSum >= target) {
+      if (right - left + 1 < minLen) {
+        minLen = right - left + 1;
+      }
+      curSum -= nums[left];
+      left++;
+    }
+  }
+
+  return minLen !== Infinity ? minLen : 0;
+};
